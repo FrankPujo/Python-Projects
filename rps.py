8043e70bbcbb0ee8ff498ed52015cab8195eac03
FrankPujo <70267733+FrankPujo@users.noreply.github.com>
2021-02-17 15:30:19 +0100

    Fix bugs, text display
    Also initializing an improved randomization

--- rps.py
+++ rps.py
@@ -1,21 +1,46 @@
 import random
 
+# check if the number of games selected is even or odd
+def even( num ):
+    half = num / 2
+    fHalf = round( half, 0 )
+    isEven = None
+    
+    if half == fHalf:
+        isEven = True
+    else:
+        isEven = False
+    
+    return isEven
+
+# better randomization for the PC choice
+def pcTurn():
+    # do something
+    print("pcTurn")
+
+# main play function
 def play():
+    # introduction to the game
     print( "\nWelcome to the \"Rock - Paper - Scissors\" game!" )
     print( "To play write 1, 2 or 3 for (respectivily) rock, paper or scissors" )
     matchesNum = input( "Choose the number of matches: " )
+    #while even( matchesNum ):
+    #   do something
     
     winCount = 0
     loseCount = 0
+    
+    # repeate matches for as many times as chosen by the player
     for x in range( int(matchesNum) ):
         print( "\nMatch no." + str(x + 1) )
         text = input( "Make your move: " )
         pcAnsw = random.randint( 1, 3 )
         choice = int( text )
         
+        # check the winner of the match
         if choice == pcAnsw:
             print( "It's a tie!")
-            print( " Current points: " + str(winCount) + " : " + str(loseCount) )
+            print( "Current points: " + str(winCount) + " : " + str(loseCount) )
         elif choice == 1 and pcAnsw == 2:
             print( "Rock against paper!" )
             print( "You lose this round!" )
@@ -28,13 +53,13 @@ def play():
             print( "Current points: " + str(winCount) + " : " + str(loseCount) )
         elif choice == 2 and pcAnsw == 1:
             print( "Paper against rock!" )
-            print( "You lose this round!" )
-            loseCount += 1
+            print( "You win this round!" )
+            winCount += 1
             print( "Current points: " + str(winCount) + " : " + str(loseCount) )
         elif choice == 2 and pcAnsw == 3:
             print( "Paper against scissors!" )
-            print( "You win this round!" )
-            winCount += 1
+            print( "You lose this round!" )
+            loseCount += 1
             print( "Current points: " + str(winCount) + " : " + str(loseCount) )
         elif choice == 3 and pcAnsw == 1:
             print( "Scissors against rock!" )
@@ -46,14 +71,16 @@ def play():
             print( "You win this round!" )
             winCount += 1
             print( "Current points: " + str(winCount) + " : " + str(loseCount) )
-        
-    if winCount >= loseCount:
+    
+    # print out the result of the game
+    if winCount > loseCount:
         print( "\nYou won " + str(winCount) + " to " + str(loseCount) )
-    elif winCount <= loseCount:
+    elif winCount < loseCount:
         print( "\nYou lost " + str(winCount) + " to " + str(loseCount) )
     elif winCount == loseCount:
         print( "\nYou tied " + str(winCount) + " to " + str(loseCount) )
     
     print( "\nThank you for playing!" )
 
+# start playing when running the code
 play()
